60f4bb8832d7cccf19bcc517a4c6c432173dc401
nlemuel <lemuelayres@gmail.com>
2024-03-21 19:10:29 -0300

    add livestream

--- main.py
+++ main.py
@@ -21,13 +21,20 @@ def RGB(event, x, y, flags, param):
 cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
 
-cap=cv2.VideoCapture('peoplecount1.mp4')
+
+
+cap = cv2.VideoCapture("http://192.168.15.3:8080/video")
+
+
+if not cap.isOpened():
+    print("Erro ao abrir a câmera")
+    exit()
 
 
 my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n") 
-#print(class_list)
+
 
 count=0
 tracker = Tracker()
@@ -47,17 +54,16 @@ while True:
     if count % 2 != 0:
         continue
     frame=cv2.resize(frame,(1020,500))
-#    frame=cv2.flip(frame,1)
+
     results=model.predict(frame)
- #   print(results)
+
     a=results[0].boxes.data
     px=pd.DataFrame(a).astype("float")
-#    print(px)
+
     list=[]
     list.clear()
              
     for index,row in px.iterrows():
-
  
         x1=int(row[0])
         y1=int(row[1])
@@ -67,23 +73,25 @@ while True:
         c=class_list[d]
         if 'person' in c:
            list.append([x1, y1, x2, y2])
+
     bbox_id = tracker.update(list)
+
     for i, bbox in enumerate(bbox_id):
         x3, y3, x4, y4, id = bbox
         results = cv2.pointPolygonTest(np.array(area2, np.int32), (x4, y4), False)
+
         if results >= 0:
             entering[id] = (x4, y4)
             cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 0, 255), 2)
         if id in entering:
             results_area1 = cv2.pointPolygonTest(np.array(area1, np.int32), (x4, y4), False)
+
             if results_area1 >= 0:
                 cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 255, 0), 2)
                 cv2.circle(frame, (x4, y4), 4, (255, 0, 255), -1)
                 cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
                 ppl_entering.add(id)
       
-            
-            
 
     #people exiting
         results_exiting = cv2.pointPolygonTest(np.array(area1, np.int32), (x4, y4), False)
@@ -101,10 +109,6 @@ while True:
                 ppl_exiting.add(id)
 
 
-
-
-
-        
     cv2.polylines(frame,[np.array(area1,np.int32)],True,(255,0,0),2)
     cv2.putText(frame,str('1'),(504,471),cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,0,0),1)
 
@@ -115,7 +119,8 @@ while True:
     cv2.putText(frame, str(inn), (60, 80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 255, 255), 2)
     cv2.putText(frame, str(out), (60, 140), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
     cv2.imshow("RGB", frame)
-    if cv2.waitKey(1)&0xFF==27:
+
+    if cv2.waitKey(1) == ord("q"):
         break
 
 cap.release()
